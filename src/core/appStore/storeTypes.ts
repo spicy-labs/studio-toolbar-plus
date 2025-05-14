f4586a1aefcafa808f8f90261341b71b33bb86d2
Sean Crowe <retrounlocked@gmail.com>
2025-05-13 20:54:26 -0500

    refactor: Rename featureName to appName

--- src/core/appStore/storeTypes.ts
+++ src/core/appStore/storeTypes.ts
@@ -3,10 +3,10 @@ import type { AppStore } from "./store";
 
 export type Store = {
   state: {
-    [featureName: string]: Record<string, any>;
+    [appName: string]: Record<string, any>;
   };
   actions: {
-    [featureName: string]: Record<string, (...args: any[]) => void>;
+    [appName: string]: Record<string, (...args: any[]) => void>;
   };
 };
 
